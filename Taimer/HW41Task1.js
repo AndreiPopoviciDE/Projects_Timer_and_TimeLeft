ecb488e181abdd03a5d90e45179514aa2500425b
Andrei-PC <andrei.popovici.de@gmail.com>
2021-11-11 16:12:33 +0100

    Update

--- Taimer/HW41Task1.js
+++ Taimer/HW41Task1.js
@@ -7,16 +7,22 @@ btnStart.onclick = start
 
 function start() {
     let count = parseInt(input.value)
-    if (isNaN(count)) return;
+    if (isNaN(count) || count == `` || count < 0) {
+        input.value = ''
+        timeover.textContent = `Pleasea only integer > 0`
+        alert("Enter correct data")
+        return
+    }
     timeover.textContent = ``
     btnStop.classList.remove("btn-disable")
     btnStart.classList.add("btn-disable")
     let id = setInterval(function () {
         timeover.textContent = `${count--}`
-        if (count < 0) {
+        if (count === 0) {
             timeover.textContent = `Time Is Over`
             input.value = ""
             clearInterval(id)
+            btnStart.classList.remove("btn-disable")
         }
 
     }, 1000);
@@ -24,7 +30,7 @@ function start() {
     btnStop.onclick = end
     function end() {
         clearInterval(id)
-        input.value = `${count--}`
+        input.value = `${count}`
         timeover.textContent = `Timer is stopped`
         btnStop.classList.add("btn-disable")
         btnStart.classList.remove("btn-disable")
